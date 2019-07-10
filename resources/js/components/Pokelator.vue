684b191b7581f65888daab135e24be4d5f2c0903
Kazuki Tobita <kazukiti201@gmail.com>
2019-07-10 10:20:29 +0900

    Fix bug

--- resources/js/components/Pokelator.vue
+++ resources/js/components/Pokelator.vue
@@ -3,8 +3,8 @@
     <h1 class="text-center">Pokelator</h1>
     <h2 class="text-center">Pokemon damage simulator</h2>
     <div class="row mb-4">
-      <input type="number" class="form-control col-md-9" v-model="keyword" autofocus>
-      <button type="button" class="col-md-3" @click="search">Search</button>
+      <input type="text" class="form-control col-md-9" v-model="keyword" @keyup.enter="search" autofocus>
+      <button type="button" class="btn btn-primary col-md-3" @click="search">Search</button>
     </div>
     <div v-if="pokemon">
       <table class="table table-striped table-bordered">
